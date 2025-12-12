ff05af7dd962558e15aab47a5df189ed8cf69063
RGD006 <mysak6288@gmail.com>
2025-12-12 23:24:07 +0200

    feat: create file mapping server

--- client/main.cpp
+++ client/main.cpp
@@ -3,39 +3,15 @@
 #include <iostream>
 #include <string>
 
-std::array<LPCTSTR, 2> slotNames = {TEXT("\\\\.\\mailslot\\sample_mailslot1"),
-                                    TEXT("\\\\.\\mailslot\\sample_mailslot2")};
-
-BOOL WriteSlot(HANDLE hSlot, LPCTSTR lpszMessage) {
-  BOOL fResult;
-  DWORD cbWritten;
-
-  fResult = WriteFile(hSlot, lpszMessage,
-                      (DWORD)(lstrlen(lpszMessage) + 1) * sizeof(TCHAR),
-                      &cbWritten, (LPOVERLAPPED)NULL);
-
-  if (!fResult) {
-    printf("WriteFile failed with %d.\n", GetLastError());
-
-    if (GetLastError() == 6) {
-      printf("Mailbox is not opened\n");
-    }
-
-    return FALSE;
-  }
-
-  printf("Slot written to successfully.\n");
-
-  return TRUE;
-}
+std::array<LPCTSTR, 2> fileNames = {TEXT("sample_file1"), TEXT("sample_file2")};
 
 int main(int argc, char **argv) {
   unsigned int client = 0;
 
   while (1) {
     std::cout << "Enter the number of client:" << std::endl
-              << "Client 1: -> 0" << std::endl
-              << "Client 2: -> 1" << std::endl;
+              << "Client 1 (sample_file1): -> 0" << std::endl
+              << "Client 2 (sample_file2): -> 1" << std::endl;
 
     std::cin >> client;
 
@@ -45,18 +21,18 @@ int main(int argc, char **argv) {
       std::cout << "Wrong client number" << std::endl;
   }
 
-  HANDLE hFile = CreateFile(slotNames[client], GENERIC_WRITE, FILE_SHARE_READ,
-                            (LPSECURITY_ATTRIBUTES)NULL, OPEN_EXISTING,
-                            FILE_ATTRIBUTE_NORMAL, (HANDLE)NULL);
+  HANDLE hFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, fileNames[client]);
 
   if (!hFile) {
-    std::cout << "Can't create file handler" << std::endl;
+    std::cout << "Can't create file handler: " << GetLastError() << std::endl;
     return -1;
   }
 
+  PVOID pBuf = MapViewOfFile(hFile, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+
   std::string message("message from: " + std::to_string(client) + " mailbox");
 
-  WriteSlot(hFile, message.c_str());
+  CopyMemory(pBuf, message.c_str(), message.size());
 
   return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -7,138 +7,69 @@
 #include <vector>
 #include <windows.h>
 
-constexpr size_t mailBoxListenTimeout = 500;
+constexpr size_t fileBufferSize = 512;
+constexpr char *empty = "";
 
-std::vector<HANDLE> tSlots;
-std::vector<HANDLE> hSlots;
-std::array<LPCTSTR, 2> slotNames = {TEXT("\\\\.\\mailslot\\sample_mailslot1"),
-                                    TEXT("\\\\.\\mailslot\\sample_mailslot2")};
+std::vector<HANDLE> tFile;
+std::vector<HANDLE> hFileMaps;
+std::array<HANDLE, 2> hFile;
+std::array<LPCTSTR, 2> fileNames = {TEXT("sample_file1"), TEXT("sample_file2")};
 
-static DWORD WINAPI ReadSlot(PVOID slot) {
-  auto mailSlot = *reinterpret_cast<std::pair<LPCSTR, size_t> *>(slot);
-  delete slot;
+static DWORD WINAPI ListenFileMap(LPVOID fileObject) {
+  auto file = *reinterpret_cast<HANDLE *>(fileObject);
 
-  if (!mailSlot.first) {
-    std::cout << "Error: no MailSlot name" << std::endl;
-    exit(1);
+  if (!file) {
+    std::cout << "Can't find file" << std::endl;
+    return -1;
   }
 
+  LPVOID buff;
+
+  buff = MapViewOfFile(file, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+
   while (1) {
-    DWORD cbMessage, cMessage, cbRead;
-    BOOL fResult;
-    LPTSTR lpszBuffer;
-    TCHAR achID[80];
-    DWORD cAllMessages;
-    HANDLE hEvent;
-    OVERLAPPED ov;
-
-    cbMessage = cMessage = cbRead = 0;
-
-    hEvent = CreateEvent(NULL, FALSE, FALSE, TEXT("ExampleSlot"));
-    if (NULL == hEvent)
-      exit(1);
-    ov.Offset = 0;
-    ov.OffsetHigh = 0;
-    ov.hEvent = hEvent;
-
-    fResult = GetMailslotInfo(hSlots[mailSlot.second], // mailslot handle
-                              (LPDWORD)NULL,  // no maximum message size
-                              &cbMessage,     // size of next message
-                              &cMessage,      // number of messages
-                              (LPDWORD)NULL); // no read time-out
-
-    if (!fResult) {
-      printf("GetMailslotInfo failed with %d.\n", GetLastError());
-      exit(1);
-    }
-
-    if (cbMessage == MAILSLOT_NO_MESSAGE) {
+    if (!buff) {
       continue;
     }
 
-    cAllMessages = cMessage;
+    if (strlen(LPCSTR(buff)) > 0) {
+      std::cout << "Message in file: " << LPCSTR(buff) << " "
+                << strlen(LPCSTR(buff)) << std::endl;
 
-    while (cMessage != 0) // retrieve all messages
-    {
-      // Create a message-number string.
-
-      StringCchPrintf((LPTSTR)achID, 80, TEXT("<Message #%d of %d>\n"),
-                      cAllMessages - cMessage + 1, cAllMessages);
-
-      // Allocate memory for the message.
-
-      lpszBuffer = (LPTSTR)GlobalAlloc(
-          GPTR, lstrlen((LPTSTR)achID) * sizeof(TCHAR) + cbMessage);
-      if (NULL == lpszBuffer) {
-        std::cout << "lpszBuffer is NULL" << std::endl;
-        exit(1);
-      }
-      lpszBuffer[0] = '\0';
-
-      fResult = ReadFile(hSlots[mailSlot.second], lpszBuffer, cbMessage,
-                         &cbRead, &ov);
-
-      if (!fResult) {
-        printf("ReadFile failed with %d.\n", GetLastError());
-        GlobalFree((HGLOBAL)lpszBuffer);
-        exit(1);
-      }
-
-      // Concatenate the message and the message-number string.
-
-      StringCbCat(lpszBuffer,
-                  lstrlen((LPTSTR)achID) * sizeof(TCHAR) + cbMessage,
-                  (LPTSTR)achID);
-
-      // Display the message.
-
-      _tprintf(TEXT("Contents of the mailslot: `%s`\n"), lpszBuffer);
-
-      GlobalFree((HGLOBAL)lpszBuffer);
-
-      fResult = GetMailslotInfo(hSlots[mailSlot.second], // mailslot handle
-                                (LPDWORD)NULL,  // no maximum message size
-                                &cbMessage,     // size of next message
-                                &cMessage,      // number of messages
-                                (LPDWORD)NULL); // no read time-out
-
-      if (!fResult) {
-        printf("GetMailslotInfo failed (%d)\n", GetLastError());
-        exit(1);
-      }
+      CopyMemory(buff, empty, fileBufferSize);
     }
-
-    Sleep(500);
-    CloseHandle(hEvent);
-  }
-}
-
-static void MakeSlot(LPCSTR slotName) {
-  hSlots.push_back(CreateMailslot(slotName, 0, MAILSLOT_WAIT_FOREVER, nullptr));
-
-  if (hSlots.back() == INVALID_HANDLE_VALUE) {
-    std::cout << "Can't craete MailSlot: " << GetLastError() << std::endl;
-    exit(1);
   }
 
-  auto *mailSlot = new std::pair<LPCSTR, size_t>(slotName, hSlots.size() - 1);
-
-  tSlots.push_back(CreateThread(nullptr, 4096, ReadSlot, mailSlot, 0, nullptr));
-  std::cout << "Create MailSlot: " << slotName << std::endl;
+  return 0;
 }
 
 Server::Server() {
-  for (const auto &slot : slotNames) {
-    MakeSlot(slot);
+  for (size_t iHandle = 0; iHandle < fileNames.size(); iHandle++) {
+
+    HANDLE hMap =
+        CreateFileMapping(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0,
+                          fileBufferSize, fileNames[iHandle]);
+
+    if (!hMap) {
+      std::cout << "CreateFileMapping error: " << GetLastError() << std::endl;
+      exit(1);
+    }
+
+    hFileMaps.push_back(hMap);
+
+    tFile.push_back(CreateThread(nullptr, 4096, ListenFileMap,
+                                 &hFileMaps[iHandle], 0, nullptr));
+
+    std::cout << "Create fileMap: " << fileNames[iHandle] << std::endl;
   }
 }
 
 void Server::run() {
-  std::cout << "Listen mailbox for 10 seconds" << std::endl;
+  std::cout << "Listen fileMaps for 10 seconds" << std::endl;
   Sleep(10 * 1000);
 
-  for (auto &mailbox : tSlots) {
-    TerminateThread(mailbox, 0);
+  for (auto &filemaps : tFile) {
+    TerminateThread(filemaps, 0);
   }
 }
 
